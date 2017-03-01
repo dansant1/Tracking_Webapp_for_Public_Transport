da4fba0e29566b04a0fa662cad8ddabab9dfc029
Farid Ivanir Escate Picon <faridescate@gmail.com>
2017-03-01 11:38:13 -0500

    agregada funcionalidad para reasignar vehiculos de despacho

--- server/main.js
+++ server/main.js
@@ -3,43 +3,43 @@ import ROLES from '../Both/Roles'
 
 Meteor.startup(() => {
 
-  	let users = [
-     	{nombre: "Usuario 1", email: "superadministrador@tpi.com.pe", roles: [ ROLES.administracion.gerente ]},
-     	{nombre: "Usuario 4", email: "soporte@tpi.com.pe", roles: [ ROLES.administracion.soporte ]},
-       	{nombre: "Usuario 2", email: "director@tpi.com.pe", roles: [ ROLES.empresa.director ]},
-       	{nombre: "Usuario 3", email: "operador@tpi.com.pe", roles: [ ROLES.empresa.operador ]}
-     ]
-
-	 _.each(users, ( user ) => {
-
-   		let id;
-
-	 	id = Accounts.createUser({
-	 	    email: user.email,
-	 	    password: "password",
-	 	    profile: { nombre: user.nombre }
-	   	})
-
-	   	switch (user.email) {
-	   		case 'superadministrador@tpi.com.pe':
-	   			Roles.addUsersToRoles(id, user.roles, ROLES.grupos.administracion);
-	   		break;
-
-	   		case 'soporte@tpi.com.pe':
-	   			Roles.addUsersToRoles(id, user.roles, ROLES.grupos.administracion);
-	   		break;
-
-	   		case 'director@tpi.com.pe':
-	   			Roles.addUsersToRoles(id, user.roles, ROLES.grupos.empresa);
-	   		break;
-
-	   		case 'operador@tpi.com.pe':
-	   			Roles.addUsersToRoles(id, user.roles, ROLES.grupos.empresa);
-	   		break;
-	   	}
-
-
-
-	 });
+  // 	let users = [
+  //    	{nombre: "Usuario 1", email: "superadministrador@tpi.com.pe", roles: [ ROLES.administracion.gerente ]},
+  //    	{nombre: "Usuario 4", email: "soporte@tpi.com.pe", roles: [ ROLES.administracion.soporte ]},
+  //      	{nombre: "Usuario 2", email: "director@tpi.com.pe", roles: [ ROLES.empresa.director ]},
+  //      	{nombre: "Usuario 3", email: "operador@tpi.com.pe", roles: [ ROLES.empresa.operador ]}
+  //    ]
+   //
+	//  _.each(users, ( user ) => {
+   //
+  //  		let id;
+   //
+	//  	id = Accounts.createUser({
+	//  	    email: user.email,
+	//  	    password: "password",
+	//  	    profile: { nombre: user.nombre }
+	//    	})
+   //
+	//    	switch (user.email) {
+	//    		case 'superadministrador@tpi.com.pe':
+	//    			Roles.addUsersToRoles(id, user.roles, ROLES.grupos.administracion);
+	//    		break;
+   //
+	//    		case 'soporte@tpi.com.pe':
+	//    			Roles.addUsersToRoles(id, user.roles, ROLES.grupos.administracion);
+	//    		break;
+   //
+	//    		case 'director@tpi.com.pe':
+	//    			Roles.addUsersToRoles(id, user.roles, ROLES.grupos.empresa);
+	//    		break;
+   //
+	//    		case 'operador@tpi.com.pe':
+	//    			Roles.addUsersToRoles(id, user.roles, ROLES.grupos.empresa);
+	//    		break;
+	//    	}
+   //
+   //
+   //
+	//  });
 
 });
